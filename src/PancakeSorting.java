1f131630728b71b4a580157cdf544343ac0e0aed
Prodigal-Gary <wenyang.huang@dingxiang-inc.com>
2019-01-06 21:13:05 -0600

    Initial commit

---
+++ src/PancakeSorting.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PancakeSorting {
+    public List<Integer> pancakeSort(int[] A) {
+        List<Integer> res = new ArrayList<>();
+        int loc = findMax(A, A.length), l = A.length;
+        while (l > 1) {
+            if (loc != 0) {
+                res.add(loc + 1);
+                flip(A, loc);
+            }
+            res.add(l);
+            flip(A, l - 1);
+            l--;
+            loc = findMax(A, l);
+        }
+        return res;
+    }
+
+    int findMax(int[] A, int index) {
+        int max = Integer.MIN_VALUE, loc = 0;
+        for (int i = 0; i < index; i++) {
+            if (A[i] > max) {
+                max = A[i];
+                loc = i;
+            }
+        }
+        return loc;
+    }
+
+    void flip(int[] arr, int i) {
+        int temp, start = 0;
+        while (start < i) {
+            temp = arr[start];
+            arr[start] = arr[i];
+            arr[i] = temp;
+            start++;
+            i--;
+        }
+    }
+}
---
+++ src/PowfulInteger.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowfulInteger {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        List<Integer> res = new ArrayList<>();
+        int m = 0, n = 0;
+        while (x != 1 && Math.pow(x, m) <= bound) m++;
+        while (y != 1 && Math.pow(y, n) <= bound) n++;
+        if (x == 1) m = 1;
+        if (y == 1) n = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = (int) Math.round(Math.pow(x, i) + Math.pow(y, j));
+                if (sum <= bound && !res.contains(sum)) res.add(sum);
+            }
+        }
+        return res;
+    }
+}
