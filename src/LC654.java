041139e33180a0293ec4a35bfd5c86f2057caae9
OriginalCoke <whuang36@hawk.iit.edu>
2019-02-15 14:57:19 -0600

    Maximum Binary Tree

---
+++ src/LC654.java
@@ -0,0 +1,26 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class LC654 {
+    //Maximum Binary Tree
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 6, 0, 5};
+        System.out.println(constructMaximumBinaryTree(nums));
+    }
+
+    public static TreeNode constructMaximumBinaryTree(int[] nums) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            TreeNode curr = new TreeNode(nums[i]);
+            while (!stack.isEmpty() && stack.peek().val < nums[i]) {
+                curr.left = stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                stack.peek().right = curr;
+            }
+            stack.push(curr);
+        }
+
+        return stack.isEmpty() ? null : stack.getLast();
+    }
+}
