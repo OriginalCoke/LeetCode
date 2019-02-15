a66e7a2cbe6aff9a500ec51fddf0dc00ee15cdf8
OriginalCoke <whuang36@hawk.iit.edu>
2019-02-15 13:07:32 -0600

    Maximum Depth of Binary Tree

---
+++ src/LC104.java
@@ -0,0 +1,12 @@
+public class LC104 {
+    //Maximum Depth of Binary Tree
+    public int maxDepth(TreeNode root) {
+        return helper(root);
+    }
+
+    public int helper(TreeNode root) {
+        if (root == null) return 0;
+        int left = helper(root.left), right = helper(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
