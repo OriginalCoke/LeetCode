c4fbe4233533bd8fecfb10f1899859b5506a34c9
OriginalCoke <whuang36@hawk.iit.edu>
2019-04-01 10:16:20 -0500

    Number of Digit One

---
+++ src/LC233.java
@@ -0,0 +1,12 @@
+public class LC233 {
+    //Number of Digit One
+    //每十个进行计算
+    public int countDigitOne(int n) {
+        int count = 0;
+        for (long k = 1; k <= n; k *= 10) {
+            long r = n / k, m = n % k;
+            count += (r + 8) / 10 * k + (r % 10 == 1 ? m + 1 : 0);
+        }
+        return count;
+    }
+}
