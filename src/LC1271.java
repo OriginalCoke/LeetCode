3356a15cf781b938cc9ec4736b27812f7799782a
OriginalCoke <whuang36@hawk.iit.edu>
2019-12-01 15:26:07 -0600

    Hexspeak

---
+++ src/LC1271.java
@@ -0,0 +1,19 @@
+public class LC1271 {
+    //Hexspeak
+    //不强转类型的做法?
+    public String toHexspeak(String num) {
+        int reminder;
+        String s = "ABCDEF";
+        StringBuilder sb = new StringBuilder();
+        long a = Long.parseLong(num);
+        while (a > 0) {
+            reminder = (int) (a % 16);
+            if (reminder > 1 && reminder < 10) return "ERROR";
+            if (reminder == 1) sb.append("I");
+            else if (reminder == 0) sb.append("O");
+            else sb.append(s.charAt(reminder - 10));
+            a /= 16;
+        }
+        return sb.reverse().toString();
+    }
+}
